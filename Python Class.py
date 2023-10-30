892b18f8ec513a00bf8adbb151fe617cb1011517
txnaachenn <130948190+txnaachenn@users.noreply.github.com>
2023-10-30 17:58:37 +1300

    Update Python Class.py

--- Python Class.py
+++ Python Class.py
@@ -16,3 +16,38 @@
 # for i in range(3)
 #     for
 # }
+
+
+family = {
+'dad': {
+'name': 'jacky',
+'age': 44},
+  
+'mom': {
+'name': 'amy',
+'age': 42},
+
+'sister': {
+'name': 'kayley',
+'age': 09},
+
+'grandma': {
+'name': 'grandma',
+'age': 67},
+
+'grandpa': {
+'name': 'grandpa',
+'age': 70},
+
+for person in family:
+  print(1)
+
+
+  
+for i in range(1, 21)
+  for j in range(1, 31)
+    if j == i or j
+      print('*', end = '')
+  else:
+    print(' ', end = '')
+  print('')
